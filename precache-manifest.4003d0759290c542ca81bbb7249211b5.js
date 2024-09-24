d5473abe562c899dc2da90d88ba6beab2c92b6f4
PARTH DOBARIYA <dobariya1212@gmail.com>
2024-09-24 14:17:30 +0530

    Updates

---
+++ precache-manifest.4003d0759290c542ca81bbb7249211b5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11906e9d4e68a0fc4cad3b7d4e4354b9",
+    "url": "/nas-v1/index.html"
+  },
+  {
+    "revision": "1674c9c1cd70da81b760",
+    "url": "/nas-v1/static/css/main.c678971d.chunk.css"
+  },
+  {
+    "revision": "12201967b3dfb4df206b",
+    "url": "/nas-v1/static/js/2.5f05d3e5.chunk.js"
+  },
+  {
+    "revision": "cbf92ed71fb4f2e94692ba67215d89b7",
+    "url": "/nas-v1/static/js/2.5f05d3e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "885cf11b9b00de7d7047",
+    "url": "/nas-v1/static/js/3.411b4446.chunk.js"
+  },
+  {
+    "revision": "1674c9c1cd70da81b760",
+    "url": "/nas-v1/static/js/main.5322e573.chunk.js"
+  },
+  {
+    "revision": "c03b55c464bab69f19bb",
+    "url": "/nas-v1/static/js/runtime-main.feb9a86d.js"
+  },
+  {
+    "revision": "ffc3aa42cc8cf4af7ffc5fceed7e7906",
+    "url": "/nas-v1/static/media/Vector.ffc3aa42.png"
+  },
+  {
+    "revision": "4b94ef85d72e0cd05cbbf8b292a0aedc",
+    "url": "/nas-v1/static/media/logo-t.4b94ef85.png"
+  },
+  {
+    "revision": "da4e8c44fee1af07d315f9e0d026e50a",
+    "url": "/nas-v1/static/media/logo.da4e8c44.jpg"
+  }
+]);
